adcd8da0695ad08c31e9447a85357e4b571153d4
supersymmetry <maildumpert@gmail.com>
2013-08-11 21:19:36 +0200

    update references/dependencies + marg docs

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject nstools "0.2.4"
+(defproject nstools "0.2.5"
   :description "Enhanced namespace management for Clojure"
   :url "http://code.google.com/p/clj-nstools/"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
 		 [org.clojure/clojure-contrib "1.2.0"]])
